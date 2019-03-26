5d63c151ccc167a2754d3232cd71395139c6bc7d
Samuel Duarte-Davies <samduarte-davies@hotmail.com>
2019-03-26 11:52:37 +0000

    navigation animates on scroll, sections sectionized

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/php/functions.php'; ?>
 
 <?php getHeader('Bellhouse Joseph - Lorem Ipsum', 'Lorm ipsum', 'page-home'); ?>
-  <section class="banner banner--sky">
-    <div class="banner__container">
+  <section class="section section--banner">
+    <div class="container">
       <h1>I'm Sam</h1>
       <p>a front-end developer.</p>
     </div>
+    <a class="section__next">GO DOWWWN</a>
   </section>
-  <section>
+  <section class="section">
 
   </section>
 
