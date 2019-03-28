53ca34b57dd2916dee86f5e6b2720b13e2f958bc
Samuel Duarte-Davies <samduarte-davies@hotmail.com>
2019-03-28 10:37:56 +0000

    centered up content, added my sxc face

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/php/functions.php'; ?>
 
 <?php getHeader('Bellhouse Joseph - Lorem Ipsum', 'Lorm ipsum', 'page-home'); ?>
-  <section class="section section--banner">
+  <section class="section section--banner section--centered">
     <div class="container">
       <h1>I'm Sam</h1>
       <p>a front-end developer.</p>
     </div>
     <a class="section__next">&nbsp;</a>
   </section>
-  <section class="section">
+  <section class="section section--hifivee">
+    Hi Five for finding my website!
     <a class="section__next">&nbsp;</a>
+  </section>
+  <section class="section section--fullheight section--centered">
+    <div class="container container--narrow">
+      <img class="img--circle" srcset="" 
+      src="/common/images/my-beautiful-face.jpg" alt="Sam Duarte-Davies' beautiful face" />
+      <h3>Hi</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
   </section>
 
 <?php getFooter(); ?>
