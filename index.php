61690338dd00015f9bbe07db6db5f4fcbd361531
Samuel Duarte-Davies <samduarte-davies@hotmail.com>
2019-04-09 10:45:16 +0100

    refined homepage popin animations

--- index.php
+++ index.php
@@ -4,37 +4,38 @@
   <section class="section section--banner section--centered section--fullheight">
     <div class="container scroll-banner">
       <div class="scroll-banner__sun"></div>
-      <h1>I'm Sam</h1>
-      <p>a front-end developer.</p>
+      <h1 class="animate--fadein duration--slow" data-emergence="hidden">I'm Sam</h1>
+      <p class="animate--fadein-up delay--05" data-emergence="hidden">a front-end developer.</p>
     </div>
     <a class="section__next section__next--blue">&nbsp;</a>
   </section>
   <section class="section section--fullheight section--centered">
-    <div class="container container--narrow animation--fadein-up" data-emergence="hidden">
-      <img class="img--circle" srcset="" 
+    <div class="container container--narrow">
+      <img class="img--circle animate--fadein" data-emergence="hidden" srcset="" 
       src="/common/images/my-beautiful-face.jpg" alt="Sam Duarte-Davies' beautiful face" />
-      <h3>Hi</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <a class="section__next">&nbsp;</a>
+      <h3 class="animate--fadein-up delay--025" data-emergence="hidden">Hi</h3>
+      <p class="animate--fadein-up delay--05" data-emergence="hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
+    <a class="section__next">&nbsp;</a>
   </section>
   <section class="section section--hifive section--fullheight">
-    <div class="hifive">
+    <div class="hifive animate--fadein-up" data-emergence="hidden">
       <div class="container">
         <h2>Hi-Five for finding my website!</h2>
       </div>
+      <div class="hifive__popup"><h2>I hope you like it!</h2></div>
       <div class="hifive__left"><span></span></div>
       <div class="hifive__right"><span></span></div>
-      <a class="section__next">&nbsp;</a>    
     </div>
+    <a class="section__next">&nbsp;</a>    
   </section>
   <section class="section section--fullheight section--centered">
-    <h2 class="animation--fadein-up" data-emergence="hidden">What I can do.</h2>
+    <h2 class="animate--fadein-up" data-emergence="hidden">What I can do.</h2>
     <div class="container container--half-half">
-      <div class="animation--fadein-left" data-emergence="hidden">
+      <div class="animate--fadein-left delay--05" data-emergence="hidden">
         Placeholder for image
       </div>
-      <div class="animation--fadein-right" data-emergence="hidden">
+      <div class="animate--fadein-right delay--05" data-emergence="hidden">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <a class="btn" href="assets/SamDuarteDavies_CV.pdf" target="_blank">Download My CV</a>
